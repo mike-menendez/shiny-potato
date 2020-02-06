b7422904f232e0d47a8a089c8e44fd468f3b2cf0
Mike Menendez <mmenendez@csumb.edu>
2020-02-05 18:27:05 -0800

    Finished code for lab 5

---
+++ labs/5/main.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+
+
+def main():
+    # mona = Image.open("mona.dat", "1")
+    d = []
+    f = open(file="mona.dat", mode="r")
+    temp = f.readlines()
+    for x in temp:
+        d.append([int (i) for i in x.replace(";", "").strip().split(" ")])
+
+    d = np.array(d)
+    d = d.astype(np.uint8)
+
+    mona = Image.fromarray(d)
+
+    mona.show(title="Mona")
+
+
+if __name__ == "__main__":
+    main()
