31185bcb19c0d2865a2df585dcbf1b9573ab08b6
casscabrera <cass.cabrera99@gmail.com>
2020-02-11 21:11:14 -0800

    fixing task2

--- labs/6/task2.py
+++ labs/6/task2.py
@@ -1,5 +1,13 @@
 from PIL import Image
-im = Image.open('/Users/casscabrera/Desktop/CST205/download.jpeg')
+
+print("Enter image path:")
+
+im = Image.open(input().strip())
+
+print("creating a sunset...")
+
 new_list = map(lambda a : (int(a[0]), int(a[1]*0.5), int(a[2]*0.5)), im.getdata())
+
 im.putdata(list(new_list))
+
 im.show()
