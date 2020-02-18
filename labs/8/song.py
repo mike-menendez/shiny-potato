f9d4f77577e1f75a578721e3946e1a13f17676b5
casscabrera <cass.cabrera99@gmail.com>
2020-02-18 13:28:57 -0800

    did lab 8, except for task3

---
+++ labs/8/song.py
@@ -0,0 +1,9 @@
+class Song:
+	def _init_(self, artist, genre, length, album):
+		self.artist = artist
+		self.genre = genre
+		self.length = length
+		self.album = album
+
+ready_to_let_go = Song("Cage The Elephant", "Indie Rock", 188, "Ready To Let Go")
+despacito = Song("Luis Fonsi", "Reggaeton", 229, "VIDA")
