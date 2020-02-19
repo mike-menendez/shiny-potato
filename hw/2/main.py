f1d106c56575d004319419388c1bf0176470dd8d
Mike Menendez <mmenendez@csumb.edu>
2020-02-19 15:38:17 -0800

    Adding starter notes for hw 2

---
+++ hw/2/main.py
@@ -0,0 +1,6 @@
+# HW: Temporal Processing of Images
+# Steps:
+#   - Read in all images
+#   - Create a list for each pixel location
+#   - Create a new image with largest combined dimensions (largest: x, y)
+#   - Insert the mode value from each list into the new image
