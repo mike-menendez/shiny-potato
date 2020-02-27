9ebadf04eb59fcf2f56afa334ea02a8fb485ccca
casscabrera <cass.cabrera99@gmail.com>
2020-02-26 16:19:48 -0800

    adding hw2 pdf

--- hw/2/main.py
+++ hw/2/main.py
@@ -10,8 +10,10 @@ import glob
 #   - Read in all images
 #   - Create a list for each pixel location
 #   - Create a new image with largest combined dimensions (largest: x, y)
-#   - Insert the mode value from each list into the new image
+#   - Insert the median value from each list into the new image
 
+#will iterate through the pixels of each image and find the median of the pixels
+#will add the median pixel to a new "final" image
 def aggregate(imgs):
 	pic = Image.new("RGB", (imgs[0].width,imgs[0].height), "white")
 	for x in range(imgs[0].width):
@@ -24,13 +26,16 @@ def aggregate(imgs):
 			val = int(len(pix)/2)
 			pic.putpixel((x,y), pix[val])
 	pic.save("images/final.png")
-	return pic
+	pic.show()
 
+#uses the python Glob library to read in multiple images from a directory
+#and will add them into a list
 def readin():
 	imgs = glob.glob("images/*")
 	imgs = [Image.open(image) for image in imgs]
 	return imgs
 
+#main function will call all functions
 def main():
     imgs = readin()
 
