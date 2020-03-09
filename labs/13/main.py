3095777ae98d917697d847f591c9acf795fcb828
Mike Menendez <mmenendez@csumb.edu>
2020-03-09 15:21:39 -0700

    Added rough draft of write up

--- labs/13/main.py
+++ labs/13/main.py
@@ -1,4 +1,4 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-print(bs(requests.get("https://www.mandatory.com/fun/1235945-nicolas-cage-memes-are-his-legacy").text, features='lxml').find("img")['src'])
+print(bs(requests.get("https://www.mandatory.com/fun/1235945-nicolas-cage-memes-are-his-legacy").text, features='lxml').find_all("img")[3]['src'])
