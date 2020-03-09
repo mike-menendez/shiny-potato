d089179b9e8f174d40866e7806804d1f707d22f9
Mike Menendez <mmenendez@csumb.edu>
2020-03-09 14:21:18 -0700

    Lab 13 completed

---
+++ labs/13/main.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+print(bs(requests.get("https://www.mandatory.com/fun/1235945-nicolas-cage-memes-are-his-legacy").text, features='lxml').find("img")['src'])
