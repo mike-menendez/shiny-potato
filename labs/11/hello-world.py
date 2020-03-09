a08a2260408d93d71ba9cf6db91bf1bf82a64bfe
casscabrera <cass.cabrera99@gmail.com>
2020-03-08 20:59:06 -0700

    lab 11 finished with pdf

--- labs/11/hello-world.py
+++ labs/11/hello-world.py
@@ -2,13 +2,24 @@
 Authors: Cassandra Cabrera & Mike Menenendez
 Date: March 9, 2020
 Purpose:
-	- satisfy Task 2 of Lab 11 for CST205
-	- creates simple Hello World website using Flask
+	- satisfy Task 3 of Lab 11 for CST205
+	- creates list of favorite colors
 '''
+from flask import Flask, render_template 
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+#defines my dictionary
+stuff = {'seasons': ['fall','winter','summer','spring'],
+		'favColors': ['forest green','dodger blue','sunshine yellow']
+		}
+
 # create an instance of the Flask class
+#links to bootstrap template as well
 app = Flask(__name__)
+Bootstrap(app)
+
 # route() decorator binds a function to a URL
 @app.route('/hello')
 def hello():
- return 'Hello world from Flask'
+	return render_template('template.html', s_list=stuff)
