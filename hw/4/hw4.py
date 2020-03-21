97910876ed5ad46721c20253437581d2d159ee68
Mike <mmenendez@csumb.edu>
2020-03-21 12:59:16 -0700

    More boilerplate for hw4

--- hw/4/hw4.py
+++ hw/4/hw4.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
-import os
-import random
+from flask_bootstrap import Bootstrap
+import os, random
 
+# Initalizes flask server with bootstrap4 since can't use mdbootstrap (cst 205 :eye_roll:)
+app = Flask(__name__)
+Bootstrap(app)
 
+# Generates sudo enum mapping based on images in child directory
 def map_gen():
     temp = os.listdir("picture")
     dict = {}
-    for i in len(temp):
+    for i in range(0, len(temp)):
         dict[i] = temp[i]
     return dict
-
 
 # Display three random images from /img directory
 @app.route('/', methods=["GET"])
@@ -18,7 +21,14 @@ def index():
     img1 = dict.pop(random.randrange(0, len(dict.keys())))
     img2 = dict.pop(random.randrange(0, len(dict.keys())))
     img3 = dict.pop(random.randrange(0, len(dict.keys())))
-    return
+    return render_template('base.html', listy=[img1, img2, img3])
 
+# TODO: Renders page based on provided image id as url parameter
+# TODO: Depends on image_info.py from instructor?????
+@app.route('/picture/<img_id>', methods=['GET'])
+def get_img(img_id):
+    pass
+
+# Start the web server in debug mode
 if __name__ == "__main__":
-    app = Flask(__name__)
+    app.run(debug=True)
