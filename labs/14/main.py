2e453d8ea6e4087e14c7358c67161184eefe37c8
Mike <mmenendez@csumb.edu>
2020-03-20 22:47:48 -0700

    Debugging lab 14, everything is correct except line 9, the code given to us is broke or I have the wrong package installed

--- labs/14/main.py
+++ labs/14/main.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
+from PIL import Image
 import numpy as np
-import requests
-import cv2
+import requests, cv2
 
-r = urlopen(bs(requests.get("https://www.mandatory.com/fun/1235945-nicolas-cage-memes-are-his-legacy").text, features='lxml').find_all("img")[3]['src'])
-image = np.asarray(bytearray(r.read()), dtype="uint8")
+r = bs(requests.get("https://www.mandatory.com/fun/1235945-nicolas-cage-memes-are-his-legacy").text, features='lxml').find_all("img")[3]['src']
+image = np.array(Image.open(requests.get(r, stream=True).raw))
 image = cv2.imdecode(image, cv2.IMREAD_COLOR)
