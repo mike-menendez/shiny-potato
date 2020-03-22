0c0626c5d1dd0b631bfe6812e0a1ef5e005e25b4
Mike <mmenendez@csumb.edu>
2020-03-22 16:03:23 -0700

    Fixed lab 14

--- labs/14/main.py
+++ labs/14/main.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
-from PIL import Image
+from urllib.request import urlopen, Request
 import numpy as np
 import requests, cv2
 
 r = bs(requests.get("https://www.mandatory.com/fun/1235945-nicolas-cage-memes-are-his-legacy").text, features='lxml').find_all("img")[3]['src']
-image = np.array(Image.open(requests.get(r, stream=True).raw))
-image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+r = bytearray(urlopen(Request(r, headers={'User-Agent': "Mozilla/5.0"})).read())
+image = cv2.imdecode(np.asarray(r, dtype="uint8"), cv2.IMREAD_COLOR)
