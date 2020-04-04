0c7183f8b47f568070f98ddd60f041752008435a
Mike <mmenendez@csumb.edu>
2020-04-03 20:42:40 -0700

    Added 404 handling

--- hw/4/hw4.py
+++ hw/4/hw4.py
@@ -1,73 +1,12 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
 from PIL import Image
-import os, random, pickle
+import os, random, pickle, image_info
 
 # Read in info from image_info
-info = [
-     {
-           "id" : "34694102243_3370955cf9_z",
-           "title" : "Eastern",
-           "flickr_user" : "Sean Davis",
-           "tags" : ["Los Angeles", "California", "building"]
-      },
-      {
-           "id" : "37198655640_b64940bd52_z",
-           "title" : "Spreetunnel",
-           "flickr_user" : "Jens-Olaf Walter",
-           "tags" : ["Berlin", "Germany", "tunnel", "ceiling"]
-      },
-      {
-           "id" : "36909037971_884bd535b1_z",
-           "title" : "East Side Gallery",
-           "flickr_user" : "Pieter van der Velden",
-           "tags" : ["Berlin", "wall", "mosaic", "sky", "clouds"]
-      },
-      {
-           "id" : "36604481574_c9f5817172_z",
-           "title" : "Lombardia, september 2017",
-           "flickr_user" : "Mónica Pinheiro",
-           "tags" : ["Italy", "Lombardia", "alley", "building", "wall"]
-      },
-      {
-           "id" : "36885467710_124f3d1e5d_z",
-           "title" : "Palazzo Madama",
-           "flickr_user" : "Kevin Kimtis",
-           "tags" : [ "Rome", "Italy", "window", "road", "building"]
-      },
-      {
-           "id" : "37246779151_f26641d17f_z",
-           "title" : "Rijksmuseum library",
-           "flickr_user" : "John Keogh",
-           "tags" : ["Amsterdam", "Netherlands", "book", "library", "museum"]
-      },
-      {
-           "id" : "36523127054_763afc5ed0_z",
-           "title" : "Canoeing in Amsterdam",
-           "flickr_user" : "bdodane",
-           "tags" : ["Amsterdam", "Netherlands", "canal", "boat"]
-      },
-      {
-           "id" : "35889114281_85553fed76_z",
-           "title" : "Quiet at dawn, Cabo San Lucas",
-           "flickr_user" : "Erin Johnson",
-           "tags" : ["Mexico", "Cabo", "beach", "cactus", "sunrise"]
-      },
-      {
-           "id" : "34944112220_de5c2684e7_z",
-           "title" : "View from our rental",
-           "flickr_user" : "Doug Finney",
-           "tags" : ["Mexico", "ocean", "beach", "palm"]
-      },
-      {
-           "id" : "36140096743_df8ef41874_z",
-           "title" : "Someday",
-           "flickr_user" : "Thomas Hawk",
-           "tags" : ["Los Angeles", "Hollywood", "California", "Volkswagen", "Beatle", "car"]
-      }
-]
+info = image_info.image_info
 
-# Initalizes flask server with bootstrap4 since can't use mdbootstrap (cst 205 :eye_roll:)
+# Initalizes flask server with bootstrap4 (psst, we went above and beyond by using animate css and a material design)
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -80,8 +19,8 @@ def index():
     img3 = imgs.pop(random.randrange(0, len(imgs) - 1))
     return render_template('index.html', listy=[img1, img2, img3])
 
-# TODO: Renders page based on provided image id as url parameter
-# TODO: Depends on image_info.py from instructor?????
+# Renders page based on provided image id as url parameter
+# Depends on image_info.py from instructor
 @app.route('/picture/<img_id>', methods=['GET'])
 def get_img(img_id):
     try:
@@ -105,7 +44,12 @@ def get_img(img_id):
         print("image not found", img_id, v)
     except Exception as e:
         print("issue with indexing on", img_id, e)
-    exit(1)
+    return render_template('404.html', linky=url_for('index'))
+
+# Yes, we have a 404 to yeet the visitor to
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', linky=url_for('index')), 404
 
 # Start the web server in debug mode
 if __name__ == "__main__":
